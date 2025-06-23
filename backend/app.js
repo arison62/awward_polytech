5e5739a307c8cf832959d5bc73fae5a39c3bf765
Albert Fotso <legrandpone1@gmail.com>
2025-06-23 02:53:53 +0100

    Correction erreur en-tete cors

--- backend/app.js
+++ backend/app.js
@@ -17,10 +17,14 @@ import studentVoteRouter from './routes/studentVote.route.js';
 import { sequelize } from './models/index.js';
 import { updateVoteStatusWithJob } from './controllers/vote.controller.js';
 
-
+const corsOptions = {
+    origin: 'https://awward-polytech.onrender.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors())
+app.use(cors(corsOptions));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
